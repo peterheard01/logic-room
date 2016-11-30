4b8e5cfdf592e7d4d6d26d70449d4969d9b1bbba
Peter Heard <peter_heard01@hotmail.com>
2016-11-30 05:08:37 +0000

    added in script to generate post properly

--- _plugins/copy_post_images.rb
+++ _plugins/copy_post_images.rb
@@ -1,15 +1,37 @@
+require 'fileutils'
 
-
-Jekyll::Hooks.register :posts, :post_render do |post|
+Jekyll::Hooks.register :posts, :post_write do |post|
 
     local_path = File.dirname(__FILE__)
     project_base_path = File.expand_path("../", local_path)
 
 
 
-   puts post.site.dest
-   puts post.destination post.site.dest
+    #puts post.site.dest
 
+    post_src_folder_path = File.dirname(post.path)
+    post_dest_file_path = post.destination post.site.dest
+
+    split_post_dest_file_path = post_dest_file_path.split("/")
+    post_folder_name = split_post_dest_file_path[split_post_dest_file_path.length - 2]
+    post_dst_folder_path = File.join(project_base_path,'_site',post_folder_name)
+
+
+    puts post_src_folder_path
+    puts post_dst_folder_path
+
+
+    FileUtils.copy_entry(post_src_folder_path, post_dst_folder_path)
+
+    #FileUtils.copy_entry('/Stuff/Dropbox/1_current/3_logic_room/_posts/1_angular_2_modules_and_components_article','/Stuff/Dropbox/1_current/3_logic_room/_site/modules-and-components-article')
+
+
+
+
+
+
+    #post_dest_path
+    #puts post_folder_name #'welcome-to-jekyll'
 
 
 
--- _posts/2016-11-19-welcome-to-jekyll.markdown
+++
@@ -1,9 +0,0 @@
----
-layout: post
-title:  Welcome to Jekyll
-date:   2016-11-19 06:56:50 +0000
-author: Pete Heard
-excerpt: Next you can update your site name, avatar and other options using the Next you can update your site name, avatar and ...
-thumbnail: 404.jpg
----
-:-)
